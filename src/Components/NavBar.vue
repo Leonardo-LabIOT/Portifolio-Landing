670dc701480db25976d20b74228a8f58f95884a9
Leonardo <leonardo_pereira1989@hotmail.com>
2023-07-08 11:53:04 -0300

    new

--- src/Components/NavBar.vue
+++ src/Components/NavBar.vue
@@ -1,43 +1,43 @@
 <template>
 	<div id="nav-container">
-
 		<div>
 			<div v-if="_mobile" @click="mobile(false)">Hello World!</div>
 			<div v-else @click="mobile(true)">Olá Mundo!</div>
-		</div>		<ion-icon name="heart" size="large"></ion-icon>
+		</div>
+		<ion-icon name="heart" size="large"></ion-icon>
 
 		<div @click="myst.inc()">{{ myst.getCount }}</div>
 	</div>
 </template>
 <script lang="ts">
-	import { useMyStore } from "@/plugins/store";
-	import { IonIcon } from "@ionic/vue";
+import { useMyStore } from "@/plugins/store";
+import { IonIcon } from "@ionic/vue";
 
-	export default {
-		components: { IonIcon },
-		setup() {
-			const myst = useMyStore();
-			return { myst };
+export default {
+	components: { IonIcon },
+	setup() {
+		const myst = useMyStore();
+		return { myst };
+	},
+	data() {
+		return {
+			_mobile: false,
+		};
+	},
+	methods: {
+		mobile(aux: boolean) {
+			this._mobile = aux;
 		},
-		data() {
-			return {
-				_mobile: false,
-			};
-		},
-		methods: {
-			mobile(aux: boolean) {
-				this._mobile = aux;
-			},
-		},
-	};
+	},
+};
 </script>
 <style scoped lang="scss">
-	#nav-container {
-		display: flex;
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		background: red;
-		// z-index: 10;
-	}
+#nav-container {
+	display: flex;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: red;
+	// z-index: 10;
+}
 </style>

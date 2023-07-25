f20759f678784f157fa2cc24f8a8d716414891ff
Leonardo <leonardo_pereira1989@hotmail.com>
2023-07-24 22:39:50 -0300

    icons fixed

--- src/Components/ToggleTheme.vue
+++ src/Components/ToggleTheme.vue
@@ -1,23 +1,24 @@
 <template>
     <div>
         <div class="themeMenu" :class="theme.get_Theme()" @click="theme.set_Theme()">
-            <ion-icon :icon="contrastOutline"></ion-icon>
-            <ion-icon :md="contrastOutline"></ion-icon>
+            <!-- <ion-icon :icon="contrastOutline"></ion-icon> -->
+            <ion-icon :icon="moon" v-if="theme.get_Theme() === 'light'"></ion-icon>
+            <ion-icon :icon="sunny" v-else></ion-icon>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
 import { useMyPrefs } from '@/plugins/store';
-import { contrastOutline } from "ionicons/icons";
+import { contrastOutline, moonOutline, sunny, moon } from "ionicons/icons";
 const theme = useMyPrefs();
 
 </script>
 <style lang="scss" >
 .themeMenu {
     display: flex;
-    align-items: end;
+    align-items: center;
     justify-content: center;
-    margin: 0 0 0 2rem;
+    margin: 0 2rem;
     padding: 0;
     color: var(--primary-color);
 
